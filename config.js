464c05c87bbfe73793b92d0c2685d7ba70e58007
Dave Sheldon <twodave@gmail.com>
2014-08-14 09:12:41 -0400

    add some bitcoin wireup

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = { 
-  "mongoString": "todo", 
-  "walletKey": "todo",
+  "mongoString": "", //just use default
+  "walletKey": "91bftUfhnuFyYy2VwedKAyH5SQ1Tq1LH7v4k3UeyMfCBszeonh2",
   "port": 80
 };
--- handlers/PaymentHandler.js
+++ handlers/PaymentHandler.js
@@ -15,7 +15,6 @@ module.exports = {
 		var query = { _id: req.params.invoice_id };
 		Invoice.findOne(query, function(err,invoice){
 			if (err) res.send(err);
-			
 			res.json(invoice.payments.id(req.params.payment_id));
 		});
 	},
@@ -27,11 +26,18 @@ module.exports = {
       var address = invoice.current_address;
 			var payment = new Payment(req.body);
       
-      // todo: make the payment
+      // make the payment
+      var key = bitcoin.ECKey.fromWIF(config.walletKey);
+      var tx = new bitcoin.Transaction();
+      tx.addInput(payment.tx_hash, 0);
+      tx.addOutput(address, amount);
+      
+      // tx.toHex();
+      // to make live, push to: https://blockchain.info/pushtx
       
       payment.address_used = address;
 			
-      invoice.current_address = 
+      invoice.current_address = key.pub.getAddress().toString(); // not sure if this is different each time
       invoice.payments.push(payment);
       
 			invoice.save(function(err, invoice){
