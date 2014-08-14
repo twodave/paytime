d4c00b3b91dedfd532d39f8806589693fc7e1e87
Dave Sheldon <twodave@gmail.com>
2014-08-14 14:02:55 -0400

    cleanup index controller code

--- assets/app/controllers/index.js
+++ assets/app/controllers/index.js
@@ -1,19 +1,50 @@
 appRoot.controller('IndexController', ['$scope', '$resource', '$rootScope', '$timeout', 'models', '$http', function ($scope, $resource, $rootScope, $timeout, models, $http) {
+  // inits
   $scope.invoice = [];
   $scope.newInvoice = new models.api.Invoice();
   $scope.newPayment = new models.api.Payment();
-  
-  $scope.getInvoices = function () {
-      return models.api.Invoice.query().then(function (data) {
-        if (data.length > 0) {
-          $scope.invoice = data[0];
-        }
-      });
-  }
-  
   $scope.flashInfo = "";
   $scope.flashError = "";
+  $scope.oneDollarInSatoshis = 0;
+  
+  // helpers
+  $scope.validInvoice = function(){
+    return $scope.invoice && $scope.invoice.expires_at && $scope.invoice.getStatus() != "Expired" && $scope.invoice.getStatus() != "Invoice Paid";
+  }
+  var getConversionRate = function (){
+    var path = "https://blockchain.info/q/24hrprice";
     
+    $http.get(path)
+      .success(function(data,status,headers,config){
+        var rate = parseFloat(data.toString());
+        $scope.oneDollarInSatoshis = (1/rate) * 100000000;
+      });
+  };
+  
+  $scope.getSatoshis = function(dollars){
+    return parseInt(dollars * $scope.oneDollarInSatoshis);
+  };
+  
+  $scope.getRunningBalance = function(index) {
+    var bal = 0;
+    if ($scope.invoice){
+      for(var i = 0; i <= index; i++){
+        bal += $scope.invoice.payments[i].amount;
+      }
+      bal = $scope.invoice.total - bal;
+    }
+    return bal;
+  }
+  
+  //api calls
+    $scope.getInvoices = function () {
+    return models.api.Invoice.query().then(function (data) {
+      if (data.length > 0) {
+        $scope.invoice = data[0];
+      }
+    });
+  }
+  
   $scope.createInvoice = function() {
     $scope.flashInfo = "";
     $scope.flashError = "";
@@ -46,43 +77,21 @@ appRoot.controller('IndexController', ['$scope', '$resource', '$rootScope', '$ti
     });
   }
   
-  $scope.oneDollarInSatoshis = 0;
-  
-  $scope.validInvoice = function(){
-    return $scope.invoice && $scope.invoice.expires_at && $scope.invoice.getStatus() != "Expired" && $scope.invoice.getStatus() != "Invoice Paid";
-  }
-  var getConversionRate = function (){
-    var path = "https://blockchain.info/q/24hrprice";
-    
-    $http.get(path)
-      .success(function(data,status,headers,config){
-        var rate = parseFloat(data.toString());
-        $scope.oneDollarInSatoshis = (1/rate) * 100000000;
-      });
-  };
   $scope.trashInvoice = function(){
+    $scope.flashInfo = "";
+    $scope.flashError = "";
     var prom = $scope.invoice.destroy().$promise.then(function(){
       $scope.invoice = [];
       $scope.getInvoices();
     });
   }
-  $scope.getSatoshis = function(dollars){
-    return parseInt(dollars * $scope.oneDollarInSatoshis);
-  };
-  $scope.getRunningBalance = function(index) {
-    var bal = 0;
-    if ($scope.invoice){
-      for(var i = 0; i <= index; i++){
-        bal += $scope.invoice.payments[i].amount;
-      }
-      bal = $scope.invoice.total - bal;
-    }
-    return bal;
-  }
+  
+  // startup
   var init = function() {
     $scope.getInvoices();
     
     getConversionRate();
   };
+  
   init();
 }]);
